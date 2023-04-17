e1ece6947b2aa7ded231d9723d7331b20af60a24
Salem <62039916+sufffix@users.noreply.github.com>
2023-04-17 10:17:41 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Character dovahkiin = new("Dovahkiin", "Fus Ro Dah!", "...");
+        Character alduin = new("Alduin", "blegh", "argh");
+
+        dovahkiin.Speak(1);
+        alduin.SetLevel(2);
+        alduin.Speak(2);
+    }
+}
+
+public class Character
+{
+    public string Name { get; set; }
+    public string Phrase1 { get; set; }
+    public string Phrase2 { get; set; }
+    public int Level { get; set; }
+
+    public Character(string name, string phrase1, string phrase2)
+    {
+        Name = name;
+        Phrase1 = phrase1;
+        Phrase2 = phrase2;
+        Level = 0;
+    }
+
+    public void Speak(int phraseNum)
+    {
+        switch (phraseNum)
+        {
+            case 1:
+                Console.WriteLine(Phrase1);
+                break;
+            case 2:
+                Console.WriteLine(Phrase2);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetLevel(int newLevel)
+    {
+        Level = newLevel;
+        Console.WriteLine("New Level: " + newLevel);
+    }
+}
